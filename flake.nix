022147d057aa6c40cb57421382e3973cf780d440
haruki7049 <tontonkirikiri@gmail.com>
2024-07-08 10:22:19 +0900

    style: nix-fmt

--- flake.nix
+++ flake.nix
@@ -6,27 +6,28 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, treefmt-nix }:
-  flake-utils.lib.eachDefaultSystem (system: 
-  let
-    pkgs = import nixpkgs { inherit system; };
-    treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
-  in {
-    formatter = treefmtEval.config.build.wrapper;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+      in
+      {
+        formatter = treefmtEval.config.build.wrapper;
 
-    checks = {
-      formatting = treefmtEval.config.build.check self;
-    };
+        checks = {
+          formatting = treefmtEval.config.build.check self;
+        };
 
-    devShells.default = pkgs.mkShell {
-      packages = with pkgs; [
-        go
-        gopls
-        nixd
-      ];
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            gopls
+            nixd
+          ];
 
-      shellHook = ''
-        export PS1="\n[nix-shell:\w]$ "
-      '';
-    };
-  });
+          shellHook = ''
+            export PS1="\n[nix-shell:\w]$ "
+          '';
+        };
+      });
 }
